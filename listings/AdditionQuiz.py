3db04b3d464c5066998165bb8bd0070f5ab0848f
Ahmed49 <158520396+ouahmedev@users.noreply.github.com>
2025-10-16 22:46:04 +0200

    Initial commit for chapter 4 listings

---
+++ listings/AdditionQuiz.py
@@ -0,0 +1,11 @@
+
+import random
+# Generate two integer numbers between 0 and 9
+number1 = random.randint(0, 9)
+number2 = random.randint(0, 9)
+
+answer = eval(input("What is " + str(number1) + " + " + str(number2) + "? "))
+
+# Display the result
+print(number1, "+", number2, "=", answer, "is", number1 + number2 == answer)
+
