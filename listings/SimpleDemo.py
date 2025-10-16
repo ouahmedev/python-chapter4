b0ea0c1b8efcdca61017f872bb6cc2c7d22a3e59
Ahmed49 <158520396+ouahmedev@users.noreply.github.com>
2025-10-16 23:38:06 +0200

    Added spaces between the percent sign and the digit

--- listings/SimpleDemo.py
+++ listings/SimpleDemo.py
@@ -3,9 +3,9 @@
 number = eval(input("Enter an integer number: "))
 
 # Check whether the number is divisible by 5
-if number %5 == 0:
+if number % 5 == 0:
     print("HiFive")
     
 # Check whether the number is even
-if number %2 == 0:
+if number % 2 == 0:
     print("HiEven")    
